11e113ef643c218f5bec0ff82014ac6a70e82dd8
Alexander Thomas <6ftDwarf@gmail.com>
2017-09-03 16:09:17 -0700

    Fix perlin noise generator

--- DynamicWorld/Perlin.cpp
+++ DynamicWorld/Perlin.cpp
@@ -24,8 +24,11 @@ void Perlin::makeGrid(float grid[], int sizex, int sizey, int seed, int scale, i
 
 	//Gradient vectors
 	//vec2 grads[sizex]; //I would really rather use this than a vector
-	int gradsizex = sizex / scale + 1; //21
-	std::vector<vec2> grads(gradsizex * (sizey / scale + 1));
+
+	float gradsizex = (float)sizex / (float)scale + 2.5f;
+	std::vector<vec2> grads(ceil( gradsizex * (((float)sizey / (float)scale) + 2.5f) ));
+	int gradSizeTotal = grads.size();
+
 	for (int i = 0; i < grads.size(); ++i)
 	{
 		grads[i] = {rand() % (heightscale * 2) - heightscale, rand() % (heightscale * 2) - heightscale };
@@ -34,66 +37,25 @@ void Perlin::makeGrid(float grid[], int sizex, int sizey, int seed, int scale, i
 	std::vector<vec2> dist(4);
 	for (int y = 0; y < sizey; ++y)
 	{
-		if (y >= 44) {
-			dist.size();
-		}
 		for (int x = 0; x < sizex; ++x)
 		{
 			//Make distance vectors: 0,0  1,0  0,1  1,1
-			if (y >= 44 && x > 98) {
-				int werlkw = 0;
-			}
 			dist[0] = { (x / scale) * scale - x,			(y / scale) * scale - y };
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			dist[1] = { (x / scale) * scale + scale - x,	(y / scale) * scale - y };
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			dist[2] = { (x / scale) * scale - x,			(y / scale) * scale + scale - y };
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			dist[3] = { (x / scale) * scale + scale - x,	(y / scale) * scale + scale - y };
 
 			//Get dot products
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			float dot00 = dot(dist[0], grads[x / scale + (y / scale) * gradsizex]);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			float dot10 = dot(dist[1], grads[x / scale + 1 + (y / scale) * gradsizex]);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			float dot01 = dot(dist[2], grads[x / scale + (y / scale + 1) * gradsizex]);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
-			float dot11 = dot(dist[3], grads[x / scale + 1 + (y / scale + 1) * gradsizex]); //46 at y=1 x=99
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
+			float dot11 = dot(dist[3], grads[x / scale + 1 + (y / scale + 1) * gradsizex]);
 
 			//Interpolate
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			float lerpy0 = lerp( { (float)((x / scale) * scale) , dot00 }, { (float)((x / scale) * scale + scale) , dot10 }, x);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
 			float lerpy1 = lerp( { (float)((x / scale) * scale) , dot01 }, { (float)((x / scale) * scale + scale) , dot11 }, x);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
-			grid[x + y * sizex] = lerp(	{ (float)((y / scale) * scale) , dot01 }, { (float)((y / scale) * scale + scale) , dot11 }, y);
-			if (y >= 44 && x > 98) {
-				dist.size();
-			}
+			grid[x + y * sizex] = lerp(	{ (float)((y / scale) * scale) , lerpy0 }, { (float)((y / scale) * scale + scale) , lerpy1 }, y);
+
 		}
 	}
 }
--- DynamicWorld/main.cpp
+++ DynamicWorld/main.cpp
@@ -421,7 +421,7 @@ void genTerrain()
 	*/
 
 	float grid[WIN_WIDTH * MAP_HEIGHT];
-	Perlin::makeGrid(grid, WIN_WIDTH, MAP_HEIGHT, 1, 5, 5);
+	Perlin::makeGrid(grid, WIN_WIDTH, MAP_HEIGHT, 1, 10, 5);
 
 	for (int x = 0; x < WIN_WIDTH; ++x)
 	{
