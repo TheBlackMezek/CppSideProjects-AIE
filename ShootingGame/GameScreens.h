f5331d56bfa96ac45fc3f79337ae59e736a0eea8
Alexander Thomas <6ftDwarf@gmail.com>
2017-09-08 11:32:45 -0700

    Remove declarations of deleted functions

--- ShootingGame/GameScreens.h
+++ ShootingGame/GameScreens.h
@@ -17,17 +17,4 @@ void switchScreenToGame();
 
 
 
-void makeCurrentOrbitImg(ElementData* e);
-void makeCargoImg(ElementData* e);
-void makeShipScreenImg(ElementData* e);
-void makeShopNameImg(ElementData* e);
-void makeShopItemImg(ElementData* e);
-void makeFuelCostImg(ElementData* e);
-void makeRetirementImg(ElementData* e);
-void makeRetireCostImg(ElementData* e);
-void makeEndImg(ElementData* e);
 
-void buyGoods(ElementData* e);
-void sellGoods(ElementData* e);
-void goToObj(ElementData* e);
-void clickRetire();
